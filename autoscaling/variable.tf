10c8ceec55e2468388f16fc1ea114c60732f30ef
yugajoshi <144673136+yugajoshi@users.noreply.github.com>
2025-07-29 15:16:26 +0530

    Create variable.tf

---
+++ autoscaling/variable.tf
@@ -0,0 +1,21 @@
+variable "instance_type" {
+    default = "t2.micro"
+}
+variable "image_id" {
+  default = "ami-0f918f7e67a3323f0"
+}
+variable "key_name" {
+    default = "public_linux"
+}
+variable "security_group_id" {
+    default = ["sg-0fc18f34fff5cd421"]
+  
+}
+variable "availability_zone" {
+    default = ["ap-south-1a"]
+  
+}
+variable "vpc_id" {
+    default = "vpc-0c7dfa3649ee39085"
+  
+}
