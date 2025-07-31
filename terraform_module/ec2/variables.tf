55287d3af5e2d64eab5d2d2d32c0c5369486a89d
yugajoshi <144673136+yugajoshi@users.noreply.github.com>
2025-07-31 15:52:57 +0530

    Create variables.tf for ec2

---
+++ terraform_module/ec2/variables.tf
@@ -0,0 +1,26 @@
+variable "subnet_id" {
+    default = ""
+  
+}
+variable "key_name" {
+    default = "public_linux"
+  
+}
+variable "vpc_id" {
+    default = ""
+  
+}
+variable "project" {
+    
+  
+}
+
+variable "image_id" {
+    default = "ami-0f918f7e67a3323f0"
+  
+}
+
+variable "instance_type" {
+    default = "t2.micro"
+  
+}
