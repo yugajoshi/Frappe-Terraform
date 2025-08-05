e98e598b77849b136c6128557a7b273bb0140ec1
yugajoshi <144673136+yugajoshi@users.noreply.github.com>
2025-08-05 15:47:05 +0530

    Create variables.tf

---
+++ terraform_module/frappe/frappe_ec2/variables.tf
@@ -0,0 +1,24 @@
+variable "project" {
+  
+}
+variable "ami_id" {
+  
+}
+variable "instance_type" {
+  
+}
+variable "public_subnet_id" {
+  
+}
+variable "public_instance_key" {
+  
+}
+variable "vpc_id" {
+  
+}
+variable "private_subnet_id" {
+  
+}
+variable "private_instance_key" {
+  
+}
