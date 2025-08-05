04bbd448a49b79af7819a9eab32785bd5c12c03d
yugajoshi <144673136+yugajoshi@users.noreply.github.com>
2025-08-05 15:43:35 +0530

    Create variables.tf

---
+++ terraform_module/frappe/frappe_vpc/variables.tf
@@ -0,0 +1,19 @@
+variable "vpc_cidr" {
+
+  
+}
+variable "public_subnet_cidr" {
+  
+}
+variable "public_subnet_az" {
+  
+}
+variable "private_subnet_cidr" {
+  
+}
+variable "private_subnet_az" {
+  
+}
+variable "project" {
+  
+}
